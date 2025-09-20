28a7f661378e7ceb270f731a05769763ea4580d2
purushottam897 <purushottamnaidu897@gmail.com>
2025-09-20 12:04:12 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Learning_Hub/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 5175,
