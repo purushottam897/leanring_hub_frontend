c8277459f55ffce323362da2f128e1e312acd151
M Purushottam Naidu <129367806+purushottam897@users.noreply.github.com>
2025-09-21 00:07:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/',
+  base: '/learning',
   plugins: [react()],
   server: {
     port: 5175,
